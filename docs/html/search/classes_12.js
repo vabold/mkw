91575b2f30e887695226f2687cef5fc175bfda63
terorie <richard@blockdaemon.com>
2022-05-14 11:51:49 +0000

    Deploying to gh-pages from @ riidefi/mkw@08cc5f960fbe31d25cd2323086328993e3b48fe1 🚀

--- docs/html/search/classes_12.js
+++ docs/html/search/classes_12.js
@@ -3,15 +3,12 @@ var searchData=
   ['random_0',['Random',['../class_util_1_1_random.html',1,'Util']]],
   ['rawghostfile_1',['RawGhostFile',['../struct_system_1_1_raw_ghost_file.html',1,'System']]],
   ['rescommon_2',['ResCommon',['../classnw4r_1_1g3d_1_1_res_common.html',1,'nw4r::g3d']]],
-  ['rescommon_3c_20const_20nw4r_3a_3ag3d_3a_3aresnamedata_20_3e_3',['ResCommon&lt; const nw4r::g3d::ResNameData &gt;',['../classnw4r_1_1g3d_1_1_res_common.html',1,'nw4r::g3d']]],
-  ['rescommon_3c_20nw4r_3a_3ag3d_3a_3acameradata_20_3e_4',['ResCommon&lt; nw4r::g3d::CameraData &gt;',['../classnw4r_1_1g3d_1_1_res_common.html',1,'nw4r::g3d']]],
-  ['rescommon_3c_20nw4r_3a_3ag3d_3a_3afogdata_20_3e_5',['ResCommon&lt; nw4r::g3d::FogData &gt;',['../classnw4r_1_1g3d_1_1_res_common.html',1,'nw4r::g3d']]],
-  ['resfont_6',['ResFont',['../structnw4r_1_1ut_1_1_res_font.html',1,'nw4r::ut']]],
-  ['resfontbase_7',['ResFontBase',['../structnw4r_1_1ut_1_1detail_1_1_res_font_base.html',1,'nw4r::ut::detail']]],
-  ['resname_8',['ResName',['../structnw4r_1_1g3d_1_1_res_name.html',1,'nw4r::g3d']]],
-  ['resnamedata_9',['ResNameData',['../structnw4r_1_1g3d_1_1_res_name_data.html',1,'nw4r::g3d']]],
-  ['rkscene_10',['RKScene',['../class_r_k_scene.html',1,'']]],
-  ['romfont_11',['RomFont',['../structnw4r_1_1ut_1_1_rom_font.html',1,'nw4r::ut']]],
-  ['runtimetypeinfo_12',['RuntimeTypeInfo',['../structnw4r_1_1ut_1_1detail_1_1_runtime_type_info.html',1,'nw4r::ut::detail']]],
-  ['rvlarchive_13',['rvlArchive',['../classrvl_archive.html',1,'']]]
+  ['resfont_3',['ResFont',['../structnw4r_1_1ut_1_1_res_font.html',1,'nw4r::ut']]],
+  ['resfontbase_4',['ResFontBase',['../structnw4r_1_1ut_1_1detail_1_1_res_font_base.html',1,'nw4r::ut::detail']]],
+  ['resname_5',['ResName',['../structnw4r_1_1g3d_1_1_res_name.html',1,'nw4r::g3d']]],
+  ['resnamedata_6',['ResNameData',['../structnw4r_1_1g3d_1_1_res_name_data.html',1,'nw4r::g3d']]],
+  ['rkscene_7',['RKScene',['../class_r_k_scene.html',1,'']]],
+  ['romfont_8',['RomFont',['../structnw4r_1_1ut_1_1_rom_font.html',1,'nw4r::ut']]],
+  ['runtimetypeinfo_9',['RuntimeTypeInfo',['../structnw4r_1_1ut_1_1detail_1_1_runtime_type_info.html',1,'nw4r::ut::detail']]],
+  ['rvlarchive_10',['rvlArchive',['../classrvl_archive.html',1,'']]]
 ];
